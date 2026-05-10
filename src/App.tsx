52313319dfd9ec2ffaa2ff59299e3c0ef179d41c
surajborana <surajborana43@gmail.com>
2026-05-10 19:26:29 +0530

    new updated

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Story from './components/Story';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Story />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
