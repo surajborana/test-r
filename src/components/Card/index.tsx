cc8d42459dbd8896be3460c95df7797581000c02
surajborana <surajborana43@gmail.com>
2026-05-12 18:18:14 +0530

    back

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,245 @@
+import { useEffect, useState } from 'react';
+import {
+  CardActionArea,
+  CardContent,
+  Card as CardM,
+  CardMedia,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  List,
+  ListItem,
+  Rating,
+  Stack,
+  Typography,
+} from '@mui/material';
+
+const recipe = {
+  id: 1,
+  name: 'Classic Margherita Pizza',
+  ingredients: [
+    'Pizza dough',
+    'Tomato sauce',
+    'Fresh mozzarella cheese',
+    'Fresh basil leaves',
+    'Olive oil',
+    'Salt and pepper to taste',
+  ],
+  instructions: [
+    'Preheat the oven to 475°F (245°C).',
+    'Roll out the pizza dough and spread tomato sauce evenly.',
+    'Top with slices of fresh mozzarella and fresh basil leaves.',
+    'Drizzle with olive oil and season with salt and pepper.',
+    'Bake in the preheated oven for 12-15 minutes or until the crust is golden brown.',
+    'Slice and serve hot.',
+  ],
+  prepTimeMinutes: 20,
+  cookTimeMinutes: 15,
+  servings: 4,
+  difficulty: 'Easy',
+  cuisine: 'Italian',
+  caloriesPerServing: 300,
+  tags: ['Pizza', 'Italian'],
+  userId: 166,
+  image: 'https://cdn.dummyjson.com/recipe-images/1.webp',
+  rating: 4.6,
+  reviewCount: 98,
+  mealType: ['Dinner'],
+};
+type Recipe = typeof recipe;
+
+const Card = () => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [recipeDetails, setRecipeDetails] = useState<Recipe | null>(null);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClickOpen = (id: number) => {
+    let data = recipes.filter((r) => r.id === id);
+    setRecipeDetails(data[0]);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const getData = async (): Promise<void> => {
+    try {
+      const respons = await fetch('https://dummyjson.com/recipes');
+
+      const data = await respons.json();
+
+      console.log(data);
+
+      // dummyjson returns { recipes: [...] }
+      setRecipes(data.recipes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <Grid container columns={{ xs: 4, sm: 8, md: 12, lg: 16 }}>
+      {recipes.map((r, index) => (
+        <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }} sx={{ padding: 1 }}>
+          <CardM
+            onClick={() => handleClickOpen(r.id)}
+            variant="outlined"
+            sx={{ marginTop: '15px' }}
+          >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                sx={{
+                  height: {
+                    xs: '140px', // mobile
+                    sm: '140px', // tablet
+                    md: '200px', // laptop
+                    lg: '200px', // desktop
+                  },
+                }}
+                image={r.image}
+                alt="Paella dish"
+              />
+              <CardContent
+                sx={{
+                  padding: {
+                    xs: 1, // mobile
+                    sm: 1, // tablet
+                    md: 2, // laptop
+                    lg: 2, // desktop
+                  },
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  component="div"
+                  sx={{
+                    fontSize: {
+                      xs: '15px', // mobile
+                      sm: '16px', // tablet
+                      md: '18px', // laptop
+                      lg: '18px', // desktop
+                    },
+                    paddingBottom: {
+                      xs: '0px', // mobile
+                      sm: '0px', // tablet
+                      md: '0px', // laptop
+                      lg: '0px', // desktop
+                    },
+                    color: '#000',
+                    fontWeight: 600,
+                  }}
+                >
+                  {r.name}
+                </Typography>
+                <Stack
+                  direction="row"
+                  spacing={0.5}
+                  sx={{
+                    alignItems: 'center',
+                    paddingBottom: 1,
+                  }}
+                >
+                  <Rating
+                    name="hal-rating-read"
+                    value={r.rating}
+                    precision={0.5}
+                    max={5}
+                    size="small"
+                    readOnly
+                  />
+                  <Typography
+                    sx={{ fontSize: '12px' }}
+                  >{`(${r.rating})`}</Typography>
+                </Stack>
+                {/*<Typography
+                  variant="body2"
+                  sx={{
+                    fontSize: {
+                      xs: '14px', // mobile
+                      sm: '14px', // tablet
+                      md: '14px', // laptop
+                      lg: '14px', // desktop
+                    },
+
+                    fontWeight: 400,
+                  }}
+                >
+                  {'Ingredients - '}
+                  {r.ingredients}
+                </Typography>*/}
+              </CardContent>
+            </CardActionArea>
+            {/*<CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>*/}
+          </CardM>
+        </Grid>
+      ))}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        role="alertdialog"
+      >
+        <DialogTitle
+          id="alert-dialog-title"
+          sx={{
+            padding: {
+              xs: '8px', // mobile
+              sm: '8px', // tablet
+              md: '16px', // laptop
+              lg: '16px', // desktop
+            },
+          }}
+        >
+          {recipeDetails && recipeDetails.name}
+        </DialogTitle>
+        <DialogContent>
+          <Typography sx={{ fontWeight: 600, marginBottom: '8px' }}>
+            {'Ingredients'}
+          </Typography>
+          <DialogContentText
+            id="alezrt-dialog-description"
+            sx={{ fontWeight: 400, fontSize: '15px', marginBottom: '8px' }}
+          >
+            {recipeDetails && recipeDetails.ingredients}
+          </DialogContentText>
+          <Typography sx={{ fontWeight: 600, marginBottom: '8px' }}>
+            {'Instructions'}
+          </Typography>
+          <DialogContentText
+            id="alert-dialog-description"
+            sx={{ fontWeight: 400, fontSize: '15px', marginBottom: '8px' }}
+          >
+            <List sx={{ padding: 0 }}>
+              {recipeDetails &&
+                recipeDetails.instructions.map((i, n) => (
+                  <ListItem
+                    disableGutters
+                    divider
+                    sx={{ display: 'list-item' }}
+                  >
+                    {i}
+                  </ListItem>
+                ))}
+            </List>
+          </DialogContentText>
+        </DialogContent>
+        {/*<DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            Disagree
+          </Button>
+          <Button onClick={handleClose}>Agree</Button>
+        </DialogActions>*/}
+      </Dialog>
+    </Grid>
+  );
+};
+
+export default Card;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  SwipeableDrawer,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const Header = () => {
+  const [state, setState] = useState({ left: false });
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, left: open });
+    };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  return (
+    <>
+      <AppBar position="sticky" sx={{ backgroundColor: '#f15b22' }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{
+              mr: 2,
+              display: {
+                xs: 'block', // mobile
+                sm: 'block', // tablet
+                md: 'block', // laptop
+                lg: 'none', // desktop
+              },
+            }}
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Recipes
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <SwipeableDrawer
+        anchor="left"
+        open={state.left}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        swipeAreaWidth={30}
+      >
+        {list()}
+      </SwipeableDrawer>
+    </>
+  );
+};
+
+export default Header;
